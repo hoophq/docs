b6eb0b060280f35a4749685153e16c9398e3ef74
Andrios Robert <andriosr@gmail.com>
2022-11-30 18:43:55 -0300

    add base url

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -9,7 +9,7 @@ const config = {
   title: 'My Site',
   tagline: 'Dinosaurs are cool',
   url: 'https://hoop.dev',
-  baseUrl: '/docs',
+  baseUrl: '/',
   onBrokenLinks: 'throw',
   onBrokenMarkdownLinks: 'warn',
   favicon: 'img/favicon.ico',
